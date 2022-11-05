052998888cb2a488fff84521bcf6597b12067fea
Momoh Paul <88063009+dumeccs@users.noreply.github.com>
2022-11-05 21:35:22 +0100

    Final commits - removed unuseful middleware

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,16 @@ const port = process.env.PORT || 3333;
 
 
 app.use(express.json());
-app.use(express.raw({ type: "application/vnd.custom-type" }));
-app.use(express.text({ type: "text/html" }));
 app.use(cors())
 
 
 app.get('/', (req, res) =>{
     try{
         res.status(200).json({
-            "slackUsername":"JerryG",
+            "slackUsername":"Pauli",
             "backend": true,
-            "age": 24,
-            "bio": "I'm a self-taught developer looking to break into tech. Javascript is my love language."
+            "age": 26,
+            "bio": "I am backend dev, and I am awesome at it."
         })
     }catch(error){
         res.status(500).json(error)
@@ -30,26 +28,25 @@ app.post('/post', (req,res) => {
   
     try{
     let result = 0
-    // switch(req.body.operation_type){
-    //     case "addition":
-    //     result = req.body.x + req.body.y 
-    //     break;
-    //     case "subtraction":
-    //     result = req.body.x - req.body.y 
-    //     break;
-    //     case "multiplication":
-    //     result = req.body.x * req.body.y 
-    //     break;
-    //     default: 
-    //     result = 0
-    // }
+    switch(req.body.operation_type){
+        case "addition":
+        result = req.body.x + req.body.y 
+        break;
+        case "subtraction":
+        result = req.body.x - req.body.y 
+        break;
+        case "multiplication":
+        result = req.body.x * req.body.y 
+        break;
+        default: 
+        result = 0
+    }
 
     console.log(req.body)
     res.status(200).json({
-        "slackUsername":"JerryG",
+        "slackUsername":"Pauli",
         "result": result,
-        "operation_type" : req.body
-        // "operation_type": req.body.operation_type
+        "operation_type": req.body.operation_type
     })
 }catch(error){
     res.status(500).json(error)
