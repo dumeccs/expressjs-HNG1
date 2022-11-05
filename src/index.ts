e37e7b1e3357ea448a90ed2b66b2e2801d679dfb
Momoh Paul <88063009+dumeccs@users.noreply.github.com>
2022-11-05 17:26:07 +0100

    test commit

--- src/index.ts
+++ src/index.ts
@@ -30,24 +30,26 @@ app.post('/', (req,res) => {
   
     try{
     let result = 0
-    switch(req.body.operation_type){
-        case "addition":
-        result = req.body.x + req.body.y 
-        break;
-        case "subtraction":
-        result = req.body.x - req.body.y 
-        break;
-        case "multiplication":
-        result = req.body.x * req.body.y 
-        break;
-        default: 
-        result = 0
-    }
-    res.status(200).json({
-        "slackUsername":"JerryG",
-        "result": result,
-        "operation_type": req.body.operation_type
-    })
+    // switch(req.body.operation_type){
+    //     case "addition":
+    //     result = req.body.x + req.body.y 
+    //     break;
+    //     case "subtraction":
+    //     result = req.body.x - req.body.y 
+    //     break;
+    //     case "multiplication":
+    //     result = req.body.x * req.body.y 
+    //     break;
+    //     default: 
+    //     result = 0
+    // }
+
+    console.log(req.body)
+    // res.status(200).json({
+    //     "slackUsername":"JerryG",
+    //     "result": result,
+    //     "operation_type": req.body.operation_type
+    // })
 }catch(error){
     res.status(500).json(error)
 }
