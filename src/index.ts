b363d8f719f4d3d183a38a3d60bc42542b6f590d
Momoh Paul <88063009+dumeccs@users.noreply.github.com>
2022-11-05 16:12:43 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,43 @@ app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
-app.get("/", async (req, res) => {
-  res.json({ "slackUsername": "Pauli", "backend": true, "age": 26, "bio": "I am a backend dev and I want to be awesome at it" });
+app.get("/",(req, res) => {
+  try{
+        res.status(200).json({ "slackUsername": "Pauli", 
+                              "backend": true, 
+                              "age": 26, 
+                              "bio": "I am a backend dev and I want to be awesome at it" }
+                            )
+    }catch(error){
+        res.status(500).json(error)
+    }
+})
 });
-
-app.use("/countries", countryRoutes);
-
+app.post('/', (req,res) => {
+  
+    try{
+    let result = 0
+    switch(req.body.operation_type){
+        case "addition":
+        result = req.body.x + req.body.y 
+        break;
+        case "subtraction":
+        result = req.body.x - req.body.y 
+        break;
+        case "multiplication":
+        result = req.body.x * req.body.y 
+        break;
+        default: 
+        result = 0
+    }
+    res.status(200).json({
+        "slackUsername":"Pauli",
+        "result": result,
+        "operation_type": req.body.operation_type
+    })
+}catch(error){
+    res.status(500).json(error)
+}
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server running on:${port}`);
 });
