0d89ae3708cbd0e1b85e23bdc9ffb716796ba025
Momoh Paul <88063009+dumeccs@users.noreply.github.com>
2022-11-05 16:15:02 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -21,31 +21,7 @@ app.get("/",(req, res) => {
     }
 })
 });
-app.post('/', (req,res) => {
-  
-    try{
-    let result = 0
-    switch(req.body.operation_type){
-        case "addition":
-        result = req.body.x + req.body.y 
-        break;
-        case "subtraction":
-        result = req.body.x - req.body.y 
-        break;
-        case "multiplication":
-        result = req.body.x * req.body.y 
-        break;
-        default: 
-        result = 0
-    }
-    res.status(200).json({
-        "slackUsername":"Pauli",
-        "result": result,
-        "operation_type": req.body.operation_type
-    })
-}catch(error){
-    res.status(500).json(error)
-}
+
 app.listen(port, () => {
   console.log(`Server running on:${port}`);
 });
