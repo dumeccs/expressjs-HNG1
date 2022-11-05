bab04db4d2825904932d9460d864a23cac091772
Momoh Paul <88063009+dumeccs@users.noreply.github.com>
2022-11-05 16:20:41 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,7 @@ app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
-app.get("/",(req, res) => {
-  try{
-        res.status(200).json({ "slackUsername": "Pauli", 
-                              "backend": true, 
-                              "age": 26, 
-                              "bio": "I am a backend dev and I want to be awesome at it" }
-                            )
-    }catch(error){
-        res.status(500).json(error)
-    }
-})
-});
+
 
 app.listen(port, () => {
   console.log(`Server running on:${port}`);
