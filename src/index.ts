245e4112098e383c0f19f9bbd062d523f75020c4
Momoh Paul <88063009+dumeccs@users.noreply.github.com>
2022-11-05 17:11:45 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,45 @@ app.use(express.text({ type: "text/html" }));
 app.use(cors())
 
 
-app.use("/", countryRoutes);
+app.get('/', (req, res) =>{
+    try{
+        res.status(200).json({
+            "slackUsername":"JerryG",
+            "backend": true,
+            "age": 24,
+            "bio": "I'm a self-taught developer looking to break into tech. Javascript is my love language."
+        })
+    }catch(error){
+        res.status(500).json(error)
+    }
+})
+
+app.post('/', (req,res) => {
+  
+    try{
+    let result = 0
+    switch(req.body.operation_type){
+        case "addition":
+        result = req.body.x + req.body.y 
+        break;
+        case "subtraction":
+        result = req.body.x - req.body.y 
+        break;
+        case "multiplication":
+        result = req.body.x * req.body.y 
+        break;
+        default: 
+        result = 0
+    }
+    res.status(200).json({
+        "slackUsername":"JerryG",
+        "result": result,
+        "operation_type": req.body.operation_type
+    })
+}catch(error){
+    res.status(500).json(error)
+}
+})
 
 app.listen(port, () => {
   console.log(`Server running on:${port}`);
