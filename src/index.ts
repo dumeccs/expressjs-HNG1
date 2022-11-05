7f96a5d4b312581fa45f9b9d1b79ed78662ddefb
Momoh Paul <88063009+dumeccs@users.noreply.github.com>
2022-11-05 17:01:47 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,12 @@ import countryRoutes from "./routes/country";
 
 const app = express();
 const port = process.env.PORT || 3333;
+const cors = require('cors');
 
 app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
-
+app.use(cors())
 
 
 app.listen(port, () => {
